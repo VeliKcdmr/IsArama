28b21e3ecc44e33b1e91352d6562d5dad0c1bd59
veli kocademir <veli.k@outlook.com>
2026-03-24 10:39:38 +0300

    Proje dosyası ekle.

---
+++ IsArama.Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace IsArama.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ IsArama.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IsArama.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
